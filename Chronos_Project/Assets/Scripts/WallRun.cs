a5f5a829a7ee512e13a24c8973cdd23076c1efcc
Dominik Marco Bien <dominik.bien@alumni.fh-aachen.de>
2021-05-03 22:50:15 +0200

    added new Scene
    Eine Scene wo der player mit einem RB gesteuert wird ( Cam,Move,Jump,!WALLRUN!)
    
    Issue: Gravity der slow fall ist so naja...

---
+++ Chronos_Project/Assets/Scripts/WallRun.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRun : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] Transform orientation;
+    [SerializeField] float minimumJumpHeight = 1.5f;
+
+
+    [Header("Detection")]
+    [SerializeField] float wallDistance = .5f;
+
+    [Header("Wall Running")]
+    [SerializeField] private float wallRunGravity;
+    [SerializeField] private float wallRunJumpForce;
+
+
+    [Header("Camera")]
+    [SerializeField] private Camera cam;
+    [SerializeField] private float fov;
+    [SerializeField] private float wallRunfov;
+    [SerializeField] private float wallRunfovTime;
+    [SerializeField] private float camTilt;
+    [SerializeField] private float camTiltTime;
+
+    public float tilt { get; private set; }
+    
+    bool wallLeft = false;
+    bool wallRight = false;
+
+    RaycastHit leftWallHit;
+    RaycastHit rightWallHit;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    bool CanWallRun()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minimumJumpHeight);
+    }
+
+    void CheckWall()
+    {
+        wallLeft = Physics.Raycast(transform.position, -orientation.right,out leftWallHit, wallDistance);
+        wallRight = Physics.Raycast(transform.position, orientation.right,out rightWallHit, wallDistance);
+    }
+
+    private void Update()
+    {
+        CheckWall();
+
+        if (CanWallRun())
+        {
+            if (wallLeft)
+            {
+                StartWallRun();
+                Debug.Log("Wall Running on the Left");
+            }
+            else if (wallRight)
+            {
+                StartWallRun();
+                Debug.Log("Wall Running on the Right");
+            }
+            else
+            {
+                StopWallRun();
+            }
+        }
+        else
+        {
+            StopWallRun();
+        }
+    }
+
+   void StartWallRun()
+    {
+        rb.useGravity = false;
+
+        rb.AddForce(Vector3.down * wallRunGravity, ForceMode.Force);
+
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, wallRunfov, wallRunfovTime*Time.deltaTime);
+        if(wallLeft)
+        {
+            tilt = Mathf.Lerp(tilt, -camTilt, camTiltTime * Time.deltaTime);
+        }
+        else if (wallRight)
+        {
+            tilt = Mathf.Lerp(tilt, camTilt, camTiltTime * Time.deltaTime);
+        }
+
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (wallLeft)
+            {
+                Vector3 walljumpDirection = transform.up + leftWallHit.normal;
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                rb.AddForce(walljumpDirection * wallRunJumpForce*100, ForceMode.Force);
+            }
+            else if (wallRight)
+            {
+                Vector3 walljumpDirection = transform.up + rightWallHit.normal;
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+                rb.AddForce(walljumpDirection * wallRunJumpForce*100, ForceMode.Force);
+            }
+        }
+    }
+
+    void StopWallRun()
+    {
+        rb.useGravity = true;
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, fov, wallRunfovTime * Time.deltaTime);
+        tilt = Mathf.Lerp(tilt, 0, camTiltTime * Time.deltaTime);
+
+    }
+}
