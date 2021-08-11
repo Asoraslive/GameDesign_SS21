f625caad59e59e8a115cd1e50013dd3b40cc9467
Dominik Marco Bien <dominik.bien@alumni.fh-aachen.de>
2021-08-11 19:26:41 +0200

    added complete Char

---
+++ Chronos_Project/Assets/HealthBarTween.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarTween : MonoBehaviour
+{
+    [SerializeField] Image fill;
+    [SerializeField] float maxHP = 100f;
+    [SerializeField] float currentHp;
+    [SerializeField] float dmg = 25f;
+    [SerializeField] Color lowLife;
+    [SerializeField] Color Life;
+    [SerializeField] float hitCd=3f;
+    [SerializeField] bool hitCdActive = false;
+    [SerializeField] bool regenLife = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHp = maxHP;
+        fill.color = Life;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (regenLife && currentHp < maxHP) lifeRegen();
+        else if (regenLife && currentHp >= maxHP) { regenLife = false; currentHp = maxHP; }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            hit(dmg);
+        }
+    }
+
+
+    public void hit(float damge)
+    {
+        //if hit restart Coroutine
+        if (hitCdActive) StopCoroutine(hitCdRoutine());
+        regenLife = false;
+        currentHp = currentHp- damge;
+        LeanTween.scale(gameObject, new Vector3(1.1f, 1.1f, 1.1f), 1f).setEaseInOutBack().setLoopPingPong(1);
+        fill.fillAmount = Mathf.Lerp(fill.fillAmount, currentHp / maxHP, 3f);
+        StartCoroutine(hitCdRoutine());
+        Debug.Log(fill.fillAmount);
+    }
+
+    private IEnumerator hitCdRoutine()
+    {
+        hitCdActive = true;
+        yield return new WaitForSeconds(hitCd);
+        hitCdActive = false;
+        regenLife = true;
+    }
+    public void lifeRegen()
+    {
+
+        currentHp += maxHP / 10 * Time.deltaTime;
+        fill.fillAmount = Mathf.Lerp(fill.fillAmount, currentHp / maxHP, 3f);
+    }
+}
