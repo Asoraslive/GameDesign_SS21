2760e0e9d017f23503aff000064b6ca6c5605913
HendrikHofer <83210367+HendrikHofer@users.noreply.github.com>
2021-08-12 16:05:47 +0200

    added projectile rotation

--- Chronos_Project/Assets/Scripts/EnemyScript/Projectile.cs
+++ Chronos_Project/Assets/Scripts/EnemyScript/Projectile.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Projectile : MonoBehaviour
 {
@@ -25,6 +26,7 @@ public class Projectile : MonoBehaviour
             return;
         }
 
+        transform.eulerAngles = convert_Vector_Quaternion(dir);
         rb.velocity = dir.normalized * speed * Time.deltaTime;
     }
 
@@ -37,5 +39,37 @@ public class Projectile : MonoBehaviour
         }
         Destroy(gameObject);
         
+    }
+
+    Vector3 convert_Vector_Quaternion(Vector3 v)
+    {
+        Vector3 x_standard = new Vector3(0, 1, 0);
+        Vector3 direction_x_projection = new Vector3(0, v.y, v.z).normalized;
+        double x_angle = Math.Acos(Vector3.Dot(x_standard, direction_x_projection));
+        float angle_x = (float)(x_angle * 180 / Math.PI);
+
+        Vector3 z_standard = new Vector3(0, 1, 0);
+        Vector3 direction_z_projection = new Vector3(0, v.y, v.z).normalized;
+        double z_angle = Math.Acos(Vector3.Dot(z_standard, direction_z_projection));
+        float angle_z = (float)(z_angle * 180 / Math.PI);
+
+        Vector3 y_standard = new Vector3(1, 0, 0);
+        Vector3 direction_y_projection = new Vector3(v.x, 0, v.z).normalized;
+        double y_angle = Math.Acos(Vector3.Dot(y_standard, direction_y_projection));
+        float angle_y = (float)(y_angle * 180 / Math.PI);
+
+        if (v.z > 0)
+        {
+            angle_y = -angle_y;
+        }
+        if (v.z < 0)
+        {
+            angle_x = -angle_x;
+        }
+        if (v.x < 0)
+        {
+            angle_z = -angle_z;
+        }
+        return new Vector3(angle_x, angle_y, angle_z);
     }
 }
