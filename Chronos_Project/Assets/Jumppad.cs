b8d2302fddb0b07f5b1b439433bec63b5705008e
Marius-Kutschke <77838963+Marius-Kutschke@users.noreply.github.com>
2021-08-13 19:43:45 +0200

    small envirment changes + Skyscraper 7

--- Chronos_Project/Assets/Jumppad.cs
+++ Chronos_Project/Assets/Jumppad.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class Jumppad : MonoBehaviour
 {
-    public float force = 20f;
+    [SerializeField] private float force = 20f;
+    [SerializeField] private bool active = false;
+
+    [SerializeField] private Material lights_deactivated;
+    [SerializeField] private Material lights_activated;
+    [SerializeField] private Material status_light;
+
+    private void Start() {
+        status_light = lights_deactivated;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.name == "Player")
-        {
-            other.GetComponent<Rigidbody>().AddForce(transform.up * force);
+        if (active) {
+            if (other.CompareTag("Player")) {
+                other.GetComponent<Rigidbody>().AddForce(transform.up * force);
+            }
+        }
+        
+    }
+
+    public void activate(bool status) {
+        active = status;
+        if (status) {
+            status_light = lights_activated;
+        }
+        else {
+            status_light = lights_deactivated;
         }
     }
 }
