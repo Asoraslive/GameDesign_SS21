5499e986994cb4d6a92410e23c6571404870f246
Marius-Kutschke <77838963+Marius-Kutschke@users.noreply.github.com>
2021-08-14 15:11:23 +0200

    updated Säule

--- Chronos_Project/Assets/Scripts/Saule_event.cs
+++ Chronos_Project/Assets/Scripts/Saule_event.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Saule_event : MonoBehaviour
 {
 
-    private bool active = false;
+    [SerializeField ]private bool active = false;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        //Quaternion rotation = Quaternion.Euler(-88, 0, 0);
+        if (active) {
+            //transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation *= Quaternion.Euler(-88, 0, 0), 5f);
+        }
     }
 
     public void startEvent() {
         active = true;
+        transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation *= Quaternion.Euler(0, -88, 0), 5000f);
     }
 }
